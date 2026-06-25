476b36222b3e7465d26c0e5ec1245edf5171a720
rafaelrisk07-maker <rafaelrisk07@gmail.com>
2026-06-25 11:43:41 -0300

    criação do bot discord
    arquivo principal do bot

---
+++ Main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online como {bot.user}")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
